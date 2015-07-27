90e5cc7b6f3065bcad8d876d244174738718be3c
Sheldon White <swhite@g2llc.com>
2015-07-27 10:03:39 -0700

    fixed a couple of encoding complaints

--- src/main/scala/euler/problem22.scala
+++ src/main/scala/euler/problem22.scala
@@ -11,7 +11,7 @@ import java.io._
 // in the list to obtain a name score.
 // For example, when the list is sorted into alphabetical order, COLIN,
 // which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
-// So, COLIN would obtain a score of 938 × 53 = 49714.
+// So, COLIN would obtain a score of 938 * 53 = 49714.
 //What is the total of all the name scores in the file?
 
 object Problem22 {
