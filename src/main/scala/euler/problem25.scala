273423716c9021af4561773b6e52ca61eeac037a
Sheldon White <swhite@g2llc.com>
2015-07-28 14:37:13 -0700

    solved problem 25

---
+++ src/main/scala/euler/problem25.scala
@@ -0,0 +1,17 @@
+package euler
+
+import scala.collection.mutable._
+import scala.math._
+import BigInt._
+import java.io._
+
+// F(1) = 1, F(2) = 1, F(3) = 2 etc.
+// What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+object Problem25 {
+	val fibs: Stream[BigInt] = BigInt(1) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+
+  def main(args: Array[String]) = {
+    var smallFibs: List[BigInt] = fibs.takeWhile({_.toString.toList.length < 1000}).toList
+    println(smallFibs.toList.length)
+  }
+}
