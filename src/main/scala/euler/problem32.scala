c47c243a6bf1e81022716f2b0b4b7a707d7f6c0c
Sheldon White <swhite@g2llc.com>
2015-07-30 13:34:01 -0700

    problem 32 completed

--- src/main/scala/euler/problem32.scala
+++ src/main/scala/euler/problem32.scala
@@ -9,9 +9,44 @@ import scala.math._
 // and product is 1 through 9 pandigital.
 // Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 // HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+
+// The only legal multipler sizes are: AA x BBB = CCCC and A x BBBB = CCCC
+// so we can limit the splits to 1,4,5 and 2,3,4
+// 1) loop through all permutations
+// 2) try the two splits
 object Problem32 {
+  val permutations = List(1,2,3,4,5,6,7,8,9).permutations
+  val matchingProducts = HashSet[Int]()
+  
+  def case1(permutation: List[Int]) = {
+      val s1 = permutation.slice(0, 1).map {x => (x + '0').toChar} mkString
+      val s2 = permutation.slice(1, 5) map {x => (x + '0').toChar} mkString
+      val s3 = permutation.slice(5, 9) map {x => (x + '0').toChar} mkString
+      val m1 = s1.mkString.toInt
+      val m2 = s2.mkString.toInt
+      val product = s3.mkString.toInt
+      if (m1 * m2 == product) {
+        matchingProducts.add(product)
+      }    
+  }
+  
+  def case2(permutation: List[Int]) = {
+      val s1 = permutation.slice(0, 2).map {x => (x + '0').toChar} mkString
+      val s2 = permutation.slice(2, 5) map {x => (x + '0').toChar} mkString
+      val s3 = permutation.slice(5, 9) map {x => (x + '0').toChar} mkString
+      val m1 = s1.mkString.toInt
+      val m2 = s2.mkString.toInt
+      val product = s3.mkString.toInt
+      if (m1 * m2 == product) {
+        matchingProducts.add(product)
+      }    
+  }
   
   def main(args: Array[String]) = {
-    //println("Problem 32: " + partitions(200, coins))    
+    for (permutation <- permutations) {
+      case1(permutation)
+      case2(permutation)
+    }
+    println("Problem 32: " + matchingProducts.sum)    
   }
 }
