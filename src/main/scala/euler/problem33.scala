af0a046099429c8d250e03860e1f3b5af5ec8c87
Sheldon White <swhite@g2llc.com>
2015-07-30 16:37:53 -0700

    solved 33

--- src/main/scala/euler/problem33.scala
+++ src/main/scala/euler/problem33.scala
@@ -12,8 +12,46 @@ import scala.math._
 // If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 
 object Problem33 {
+  var permutations = for (a <- 1 to 9; b <- 1 to 9; c <- 1 to 9) yield (a, b, c)
+  var solutions = ListBuffer[Tuple2[Int, Int]]()
+  
+  def checkPermutation(combination: Tuple3[Int, Int, Int]) = {
+    val a = combination._1
+    val b = combination._2
+    val c = combination._3
+    // case 1: ab/bc = a/c (a < b)
+    if (a < b && b != 0) {
+      val t1 = (10*a + b).toFloat
+      val b1 = (10*b + c).toFloat
+      val t2 = a.toFloat
+      val b2 = c.toFloat
+      val q1 = t1 / b1
+      val q2 = t2 / b2
+      if (q1 == q2) {
+        println("case 1")
+        solutions += Tuple2(t1.toInt, b1.toInt)
+      }
+    }
+    // case 2: ab/ca = b/c (a < c)
+    if (a < c && c != 0) {
+      val t1 = (10*a + b).toFloat
+      val b1 = (10*c + a).toFloat
+      val t2 = b.toFloat
+      val b2 = c.toFloat
+      val q1 = t1 / b1
+      val q2 = t2 / b2
+      if (q1 == q2) {
+        println("case 2")
+        solutions += Tuple2(t1.toInt, b1.toInt)
+      }
+    }
+  }
   
   def main(args: Array[String]) = {
-    //println("Problem 33: " + matchingProducts.sum)    
+    permutations foreach checkPermutation
+    val numerator = solutions.foldLeft(1) {_ * _._1}
+    val denominator = solutions.foldLeft(1) {_ * _._2}
+    solutions foreach println
+    println("Problem 33: " + denominator / numerator)    
   }
 }
